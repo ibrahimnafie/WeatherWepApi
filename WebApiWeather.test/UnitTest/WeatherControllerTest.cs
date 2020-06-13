6498fc8f1f3d739b1a6742a8cd88672f414d6bdc
ibrahimnafie <dev.inafea@hotmail.com>
2020-06-13 20:12:34 +0300

    add solution to git

---
+++ WebApiWeather.test/UnitTest/WeatherControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WebApiWeather.Model;
+using Xunit;
+
+namespace WebApiWeather.test.UnitTest
+{
+    public class WeatherControllerTest
+    {
+        [Fact]
+        public void Check_Type_Of_Response()
+        {
+            //Arrange
+            int DayOrder = 1;
+
+            //Act
+            var response = WeatherBusiness.GetDayWeather(DayOrder);
+
+            //Assert
+
+            Assert.IsType<WeatherForecast>(response);
+        }
+    }
+}
---
+++ WebApiWeather/Model/WeatherBusiness.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiWeather.Model
+{
+    public class WeatherBusiness
+    {
+        private static readonly string[] Summaries = new[]
+          {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        public static WeatherForecast GetDayWeather(int dayorder)
+        {
+            var rng = new Random();
+            var oneday = new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(dayorder),
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                TemperatureC = rng.Next(-20, 55),
+            };
+            return oneday;
+        }
+    }
+}
